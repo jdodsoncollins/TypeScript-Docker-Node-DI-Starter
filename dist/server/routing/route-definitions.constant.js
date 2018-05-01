b7017edfa8cfe01fd7976d5732a64e3f64b06dbc
Jeremy Collins <jdodsoncollins@gmail.com>
2018-05-01 15:51:44 -0700

    Remove dist

--- dist/server/routing/route-definitions.constant.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.RouteDefinitions = {
-    'user': '/users',
-    'user.update': '/users/update',
-};
-//# sourceMappingURL=route-definitions.constant.js.map
